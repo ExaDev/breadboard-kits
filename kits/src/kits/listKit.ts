0f081aa5967057f94a5bf3c8b55794eaa160b513
Joseph Mearman <joseph@mearman.co.uk>
2023-11-11 16:20:20 +0000

    Update exports in index.ts and kits/index.ts
    - Updated the exports in `index.ts` to include all exports from `types/index.js` and `kits/index.js`.
    - Updated the exports in `kits/index.ts` to include all exports from `types/index.js` and `kits/index.js`.

--- kits/src/kits/listKit.ts
+++ kits/src/kits/listKit.ts
@@ -1,61 +1,52 @@
-import { InputValues, NodeValue } from "@google-labs/breadboard";
 import { KitBuilder } from "@google-labs/breadboard/kits";
+import {
+	ListConcatInput,
+	ListIndexInput,
+	ListInput,
+	ListItemInput,
+	ListOperationOutput,
+	ListOutput,
+	ListSplicOutput,
+	ListSpliceInput,
+	SplitListOutput,
+} from "index.js";
 
 const ListKit = new KitBuilder({
 	url: "npm:@exadev/breadboard-kits/list",
 }).build({
 	/* eslint-disable @typescript-eslint/require-await */
-	concat: async (
-		inputs: InputValues & { a: NodeValue[]; b: NodeValue[] }
-	): Promise<void | Partial<Record<string, NodeValue>>> => {
+	concat: async (inputs: ListConcatInput): Promise<ListInput> => {
 		const { a, b } = inputs;
 		return { list: a.concat(b) };
 	},
-	split: async (
-		inputs: InputValues & { list: NodeValue[]; index: number }
-	): Promise<InputValues & { before: NodeValue[]; after: NodeValue[] }> => {
+	split: async (inputs: ListIndexInput): Promise<SplitListOutput> => {
 		const { list, index } = inputs;
 		return {
 			before: list.slice(0, index),
 			after: list.slice(index),
 		};
 	},
-	push: async (
-		inputs: InputValues & { list: NodeValue[]; item: NodeValue }
-	): Promise<void | Partial<Record<string, NodeValue>>> => {
+	push: async (inputs: ListItemInput): Promise<ListInput> => {
 		const { list, item } = inputs;
 		list.push(item);
 		return { list };
 	},
-	shift: async (
-		inputs: InputValues & { list: NodeValue[] }
-	): Promise<InputValues & { item: NodeValue; list: NodeValue[] }> => {
+	shift: async (inputs: ListInput): Promise<ListOperationOutput> => {
 		const { list } = inputs;
 		const item = list.shift();
 		return { item, list };
 	},
-	pop: async (
-		inputs: InputValues & { list: NodeValue[] }
-	): Promise<InputValues & { item: NodeValue; list: NodeValue[] }> => {
+	pop: async (inputs: ListInput): Promise<ListOperationOutput> => {
 		const { list } = inputs;
 		const item = list.pop();
 		return { item, list };
 	},
-	unshift: async (
-		inputs: InputValues & { list: NodeValue[]; item: NodeValue }
-	): Promise<void | Partial<Record<string, NodeValue>>> => {
+	unshift: async (inputs: ListItemInput): Promise<ListOutput> => {
 		const { list, item } = inputs;
 		list.unshift(item);
 		return { list };
 	},
-	splice: async (
-		inputs: InputValues & {
-			list: NodeValue[];
-			start: number;
-			count: number;
-			items?: NodeValue[];
-		}
-	): Promise<InputValues & { extracted: NodeValue[]; list: NodeValue[] }> => {
+	splice: async (inputs: ListSpliceInput): Promise<ListSplicOutput> => {
 		const { list, start, count, items } = inputs;
 		const extracted = list.splice(start, count, ...(items ?? []));
 		return { extracted, list };
---
+++ kits/src/types/list.ts
@@ -0,0 +1,45 @@
+import { InputValues, NodeValue, OutputValues } from "@google-labs/breadboard";
+
+export type ListOperationOutput = OutputValues & {
+	item: NodeValue;
+	list: NodeValue[];
+};
+
+export type ListInput = InputValues & {
+	list: NodeValue[];
+};
+export type ListOutput = OutputValues & {
+	list: NodeValue[];
+};
+
+export type SplitListOutput = OutputValues & {
+	before: NodeValue[];
+	after: NodeValue[];
+};
+
+export type ListConcatInput = InputValues & {
+	a: NodeValue[];
+	b: NodeValue[];
+};
+
+export type ListIndexInput = InputValues & {
+	list: NodeValue[];
+	index: number;
+};
+
+export type ListItemInput = InputValues & {
+	list: NodeValue[];
+	item: NodeValue;
+};
+
+export type ListSpliceInput = InputValues & {
+	list: NodeValue[];
+	start: number;
+	count: number;
+	items?: NodeValue[];
+};
+
+export type ListSplicOutput = OutputValues & {
+	extracted: NodeValue[];
+	list: NodeValue[];
+};
