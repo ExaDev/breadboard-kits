112fe1ec65ea52123ca5cfbcef12591c2d76db88
Joseph Mearman <joseph@mearman.co.uk>
2023-11-13 10:47:58 +0000

    Add new test for listKit.pop function

---
+++ tests/listkit.pop.ava.ts
@@ -0,0 +1,37 @@
+import { Board } from "@google-labs/breadboard";
+import test from "ava";
+import ListKit from "../src/kits/listKit.js";
+test("listKit.pop", async (t) => {
+	const board: Board = new Board();
+	const listKit = board.addKit(ListKit);
+
+	const list = board.input({
+		$id: "list",
+		schema: {
+			type: "object",
+			properties: {
+				list: {
+					type: "array",
+					title: "list",
+					description: "list",
+				},
+			},
+		},
+		inputs: {
+			list: ["a", "b", "c"],
+		},
+	});
+
+	const pop = listKit.pop();
+
+	list.wire("->list", pop);
+	pop.wire("->list", pop);
+	const output = board.output();
+	pop.wire("->item", output);
+
+	const result = await board.runOnce({
+		list: ["a", "b", "c"],
+	});
+	console.log(result);
+	t.is(result["item"], "c");
+});
