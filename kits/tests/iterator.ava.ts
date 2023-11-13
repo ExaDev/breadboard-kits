70a4c48ae6b71b538bd0ff85644dfe1dc08d1b4b
Joseph Mearman <joseph@mearman.co.uk>
2023-11-13 13:19:46 +0000

    Add iterator test for ListKit
    This commit adds a new test file `iterator.ava.ts` that tests the iterator functionality of the ListKit module. The test creates a board, adds a ListKit, and sets up the necessary wiring to iterate over an input list. It then runs the board and collects the output items in an array. Finally, it asserts that the length of the accumulated results matches the length of the input array and that the input array is empty after iteration.
    
    Note: The console.log statements used for debugging will be removed before merging into main branch.

---
+++ kits/tests/iterator.ava.ts
@@ -0,0 +1,49 @@
+import { Board, LogProbe } from "@google-labs/breadboard";
+import test from "ava";
+import ListKit from "../src/kits/listKit.js";
+test("iterator", async (t) => {
+	const board: Board = new Board();
+	const listKit = board.addKit(ListKit);
+
+	const list = board.input({
+		$id: "list",
+		schema: {
+			type: "object",
+			properties: {
+				list: {
+					type: "array",
+					title: "list",
+					description: "list",
+				},
+			},
+		},
+	});
+
+	const pop = listKit.pop();
+
+	list.wire("->list", pop);
+	pop.wire("->list", pop);
+	const output = board.output();
+	pop.wire("item->", output);
+
+	const testArray = ["a", "b", "c", "d", "e"];
+	const arrayLength = testArray.length;
+	console.log("testArray:", testArray);
+	const accumulatedResults = [];
+	for await (const result of board.run({
+		probe: new LogProbe(),
+	})) {
+		console.log("\n------------------------------\n");
+		if (result.type === "input") {
+			result.inputs = {
+				list: testArray,
+			};
+		} else if (result.outputs && result.outputs.item) {
+			const item = result.outputs.item;
+			accumulatedResults.push(item);
+		}
+	}
+	console.log("RESULT:", accumulatedResults);
+	t.is(accumulatedResults.length, arrayLength);
+	t.is(testArray.length, 0);
+});
