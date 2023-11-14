e13b35b84ea3fb7b0173c6dd6ae67459f2e1d8d1
Joseph Mearman <joseph@mearman.co.uk>
2023-11-14 14:05:03 +0000

    Refactor util.ts: - Remove unused import of "path" - Export functions writeFiles, generateAndWriteJson, writeJson, generateJson, generateAndWriteMarkdown, writeMarkdown, and generateCombinedMarkdown - Simplify the return statement in generateCombinedMarkdown function

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,6 @@
-import path from "path";
 import { Board } from "@google-labs/breadboard";
 import fs from "fs";
+import path from "path";
 
 export function writeCombinedMarkdown(dir: string, name: string, markdownTemplate: string) {
 	const mdFilepath = path.resolve(path.join(dir, "markdown", `${name}.md`));
@@ -10,7 +10,7 @@ export function writeCombinedMarkdown(dir: string, name: string, markdownTemplat
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function writeFiles(board: Board, name = board.title, dir = "./output") {
+export function writeFiles(board: Board, name = board.title, dir = "./output") {
 	if (!name) {
 		throw new Error("Board must have a title or a name must be suplied");
 	}
@@ -19,35 +19,35 @@ function writeFiles(board: Board, name = board.title, dir = "./output") {
 	generateAndWriteJson(dir, name, board);
 }
 
-function generateAndWriteJson(dir: string, name: string, board: Board) {
+export function generateAndWriteJson(dir: string, name: string, board: Board) {
 	const jsonContent = generateJson(board);
 	writeJson(dir, name, jsonContent);
 }
 
-function writeJson(dir: string, name: string, jsonContent: string) {
+export function writeJson(dir: string, name: string, jsonContent: string) {
 	const jsonFilePath = path.resolve(path.join(dir, "json", `${name}.json`));
 	fs.mkdirSync(path.dirname(jsonFilePath), { recursive: true });
 	fs.writeFileSync(jsonFilePath, jsonContent);
 	console.log("wrote", `"${jsonFilePath}"`);
 }
 
-function generateJson(board: Board) {
+export function generateJson(board: Board) {
 	return JSON.stringify(board, null, "\t");
 }
 
-function generateAndWriteMarkdown(dir: string, name: string, board: Board) {
+export function generateAndWriteMarkdown(dir: string, name: string, board: Board) {
 	const markdown = generateMarkdown(board);
 	writeMarkdown(dir, name, markdown);
 }
 
-function writeMarkdown(dir: string, name: string, markdown: string) {
+export function writeMarkdown(dir: string, name: string, markdown: string) {
 	const mdFilepath = path.resolve(path.join(dir, "markdown", `${name}.md`));
 	fs.mkdirSync(path.dirname(mdFilepath), { recursive: true });
 	fs.writeFileSync(mdFilepath, markdown);
 	console.log("wrote", `"${mdFilepath}"`);
 }
 
-function generateMarkdown(board: Board) {
+export function generateMarkdown(board: Board) {
 	return `\`\`\`mermaid\n${board.mermaid()}\n\`\`\``;
 }
 
@@ -60,7 +60,7 @@ export function generateAndWriteCombinedMarkdown(board: Board, name = board.titl
 	writeCombinedMarkdown(dir, name, markdownTemplate);
 }
 
-function generateCombinedMarkdown(board: Board, name: string = board.title) {
+export function generateCombinedMarkdown(board: Board, name: string = board.title) {
 	if (!name) {
 		throw new Error("Board must have a title or a name must be suplied");
 	}
@@ -68,6 +68,5 @@ function generateCombinedMarkdown(board: Board, name: string = board.title) {
 	const markdown = generateMarkdown(board);
 	const json = generateJson(board);
 	const jsonCodeBlock = `\`\`\`json\n${json}\n\`\`\``;
-	const markdownTemplate = `# ${name}\n\n${markdown}\n\n${jsonCodeBlock}`;
-	return markdownTemplate;
+	return `# ${name}\n\n${markdown}\n\n${jsonCodeBlock}`;
 }
