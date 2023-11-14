393e6633529da6ceedd5f1b71be29cb67753eac3
Joseph Mearman <joseph@mearman.co.uk>
2023-11-14 11:20:57 +0000

    Add test for getting specific story attributes
    This commit adds a new test case to retrieve specific attributes of a story from Hacker News using Algolia. The test sets up the necessary dependencies and wires them together to fetch the required data. It then asserts that the expected attributes are present in the output.

--- tests/hn.algolia.ava.ts
+++ tests/hn.algolia.ava.ts
@@ -36,3 +36,47 @@ test("hackernews algolia get story by id", async (t: ExecutionContext) => {
 		}
 	}
 })
+
+test("Get specific story attributes", async (t: ExecutionContext) => {
+	const board = new Board();
+	const firebase = board.addKit(HnFirebaseKit);
+	const algolia = board.addKit(HackerNewsAlgoliaKit);
+	// const output = board.output();
+	const listKit = board.addKit(ListKit);
+
+	const pop = listKit.pop();
+
+	const topStoryIdNode = firebase.topStoryIds({
+		limit: 2,
+	});
+
+	const getStoryFromId = algolia.getStory();
+	topStoryIdNode.wire("storyIds->list", pop);
+	pop.wire("->list", pop);
+	pop.wire("item->id", getStoryFromId);
+
+	const core = board.addKit(Core);
+
+	const aggregate = core.passthrough();
+
+	getStoryFromId.wire("->algoliaUrl", aggregate);
+	getStoryFromId.wire("->story_id", aggregate);
+	getStoryFromId.wire("->url", aggregate);
+	getStoryFromId.wire("->title", aggregate);
+
+	aggregate.wire("*->", board.output());
+
+	for await (const result of board.run({
+		// probe: new LogProbe(),
+	})) {
+		console.log("------");
+		if (result.outputs) {
+			const outputs = result.outputs;
+			console.log("outputs:", JSON.stringify(outputs, null, 2));
+			t.assert(outputs.algoliaUrl, "outputs.algoliaUrl");
+			t.assert(outputs.story_id, "outputs.story_id");
+			t.assert(outputs.url, "outputs.url");
+			t.assert(outputs.title, "outputs.title");
+		}
+	}
+});
