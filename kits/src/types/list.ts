742770d5176ad280259470941c156a004948ee32
jig925 <57284752+jig925@users.noreply.github.com>
2023-11-15 15:27:40 +0000

    Added Tests, fixed bugs, added documentation
    Added Tests for all listKit functions
    
    Added documentation for listKit functions
    
    Added functionality for some RegEx usage
    
    Fixed a bug when trim_items flag is set to true and wasn't trimming items
    
    Fixed a bug when keep_delimiters flag is set to true and wasn't keeping delimiters
    
    Fixed a bug when remove_empty_text flag is set to true and wasn't removing empty text
    
    TODO implement keep_delimiters when using RegEx

--- kits/src/types/list.ts
+++ kits/src/types/list.ts
@@ -42,10 +42,14 @@ export type ListSpliceOutput = OutputValues & List & {
 	extracted: NodeValue[];
 };
 
+// only allow a delimeter OR a regex (has to be string) because nodeValue doesn't allow RegEx types
+export type splitDelimiter = 
+ ({ delimiter: string, regex?: never } | { regex: string, delimiter?: never}) ;
+
+
 export type SplitInput = InputValues & {
 	input: string;
-	delimiter: RegExp | string;
-	trim: boolean;
+	delimiter: splitDelimiter;
 	split_by_each: boolean; // split_by_each - [ OPTIONAL - TRUE by default ] - Whether or not to divide text around each character contained in delimiter.
 	remove_empty_text: boolean; // [ OPTIONAL - TRUE by default ] - Whether or not to remove empty text messages from the split results. The default behavior is to treat consecutive delimiters as one (if TRUE). If FALSE, empty cells values are added between consecutive delimiters.
 	trim_items: boolean; // [ OPTIONAL - FALSE by default ] - Whether or not to trim each item in the split results.
