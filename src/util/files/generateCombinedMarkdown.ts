aa1ad46227076ac13e36f0b96d7e61b0bdffdc92
Joseph Mearman <joseph@mearman.co.uk>
2023-11-16 23:49:41 +0000

    Refactor generateCombinedMarkdown to allow for an undefined name parameter

--- src/util/files/generateCombinedMarkdown.ts
+++ src/util/files/generateCombinedMarkdown.ts
@@ -2,7 +2,7 @@ import { Board } from "@google-labs/breadboard";
 import generateJson from "./generateJson.js";
 import generateMarkdown from "./generateMarkdown.js";
 
-export default function generateCombinedMarkdown(board: Board, name: string = board.title) {
+export default function generateCombinedMarkdown(board: Board, name: string | undefined = board.title) {
 	if (!name) {
 		throw new Error("Board must have a title or a name must be suplied");
 	}
