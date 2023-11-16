25754fccab1ea5df7d9ed82f54367d437d9547e6
Joseph Mearman <joseph@mearman.co.uk>
2023-11-16 21:34:11 +0000

    Refactor import statements for generateAndWrite functions
    - Refactored the import statements in `generateAndWriteCombinedMarkdown.ts`, `generateAndWriteJson.ts`, `generateAndWriteMarkdown.ts`, and `writeFiles.ts` to use individual file imports instead of importing from the index file.
    - This improves code readability and maintainability.

--- src/util/files/generateAndWriteJson.ts
+++ src/util/files/generateAndWriteJson.ts
@@ -1,5 +1,6 @@
 import { Board } from "@google-labs/breadboard";
-import { generateJson, writeJson } from "./index.js";
+import generateJson from "./generateJson.js";
+import writeJson from "./writeJson.js";
 
 export default function generateAndWriteJson(dir: string, name: string, board: Board) {
 	const jsonContent = generateJson(board);
--- src/util/files/generateAndWriteMarkdown.ts
+++ src/util/files/generateAndWriteMarkdown.ts
@@ -1,5 +1,6 @@
 import { Board } from "@google-labs/breadboard";
-import { generateMarkdown, writeMarkdown } from "./index.js";
+import generateMarkdown from "./generateMarkdown.js";
+import writeMarkdown from "./writeMarkdown.js";
 
 export default function generateAndWriteMarkdown(dir: string, name: string, board: Board) {
 	const markdown = generateMarkdown(board);
