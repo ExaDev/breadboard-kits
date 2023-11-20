fbd390de02b5013b5bf31cd9ca50b55b54214e22
jig925 <57284752+jig925@users.noreply.github.com>
2023-11-20 15:28:55 +0000

    MarkDownGenerator
    WIP
    
    Started to implement top level function which handles all markdown generation

---
+++ kits/src/util/files/generateAndWriteMarkdowns.ts
@@ -0,0 +1,32 @@
+import { Board } from "@google-labs/breadboard";
+import { MarkdownConfig } from "../../types/markdown.js"
+import { generateAndWriteMermaidMarkdown } from "./generateAndWriteMermaidMarkdown.js"
+import { generateAndWriteJson } from "./generateAndWriteJson.js"
+import { generateAndWriteCombinedMarkdown } from "./generateAndWriteCombinedMarkdown.js"
+import { writeFiles } from "./writeFiles.js"
+
+export default function generateAndWriteMarkdowns(
+	{ board, filename, title, dir, markdownConfig }: {
+		board: Board,
+		filename: string,
+		title: string,
+		dir: string,
+		markdownConfig: MarkdownConfig
+	}
+) {
+	if (markdownConfig.mermaidMarkdown && !markdownConfig.jsonMarkdown) {
+		generateAndWriteMermaidMarkdown({ dir, filename, board, title })
+	} else if (markdownConfig.jsonMarkdown && !markdownConfig.mermaidMarkdown) {
+		generateAndWriteJson({ dir, filename, board })
+	} else if (markdownConfig.jsonMarkdown && markdownConfig.mermaidMarkdown) {
+		if (markdownConfig.combined) {
+			generateAndWriteCombinedMarkdown({ board, filename, title, dir })
+		} else {
+			writeFiles(board, filename, dir)
+		}
+	} else {
+		throw new Error("generateAndWriteMarkdowns Error: markdownConfig configured to generate no markdowns")
+	}
+}
+
+export { generateAndWriteMarkdowns }
