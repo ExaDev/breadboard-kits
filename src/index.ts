476cf5f5be0b8a7526925ef3f7c8b8511b5e4be0
Joseph Mearman <joseph@mearman.co.uk>
2023-11-21 13:37:16 +0000

    Add additional exports to index.ts
    This commit adds additional exports to the `index.ts` file, including `ConfigKit`, `HackerNewsAlgoliaKit`, `HackerNewsFirebaseKit`, `JsonKit`, `ListKit`, `ObjectKit`, and `StringKit`. These exports are now available for use in other modules.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 export * as kits from "./kits/index.js";
 export * as types from "./types/index.js";
 export * as util from "./util/index.js";
+
+export {
+	ConfigKit,
+	HackerNewsAlgoliaKit,
+	HackerNewsFirebaseKit,
+	JsonKit,
+	ListKit,
+	ObjectKit,
+	StringKit
+} from "./kits/index.js";
