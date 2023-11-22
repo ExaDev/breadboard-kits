c3f76c5cd16ee9c381930662160a67b3b7d6ba08
Joseph Mearman <joseph@mearman.co.uk>
2023-11-22 06:41:37 +0000

    Add search functionality to HackerNewsAlgoliaKit
    This commit adds the `search` method to the `HackerNewsAlgoliaKit` class, allowing users to search for posts based on various parameters such as query, tags, numeric filters, and page number. The method constructs the appropriate URL with query parameters and fetches the results from the Hacker News Algolia API.

--- src/kits/hackerNews/HackerNewsAlgoliaKit.ts
+++ src/kits/hackerNews/HackerNewsAlgoliaKit.ts
@@ -23,10 +23,33 @@ export type Story = PostItem & {
 	url: string;
 };
 
-const HackerNewsAlgoliaKit = new KitBuilder({
+export type HackerNewsAlgoliaSearchTags =
+	| "story"
+	| "comment"
+	| "poll"
+	| "pollopt"
+	| "show_hn"
+	| "ask_hn"
+	| "front_page";
+export type NumericFilterField = "created_at_i" | "points" | "num_comments";
+export type Operator = "<" | "<=" | "=" | ">" | ">=";
+
+export type HackerNewsSearchNumericFilters = {
+	operator: Operator;
+	field: NumericFilterField;
+	value: number;
+};
+export type HackerNewAlgoliaSearchParameters = {
+	query: string;
+	tags?: HackerNewsAlgoliaSearchTags[];
+	numericFilters?: HackerNewsSearchNumericFilters[];
+	page?: number;
+};
+
+export const HackerNewsAlgoliaKit = new KitBuilder({
 	url: "npm:@exadev/breadboard-kits/HackerNewsAlgoliaKit",
 }).build({
-	getStory: async (inputs: InputValues): Promise<OutputValues & Story> => {
+	async getStory(inputs: InputValues): Promise<OutputValues & Story> {
 		const id: string = inputs.id as string;
 		const url = `https://hn.algolia.com/api/v1/items/${id}`;
 		// return {url}
@@ -37,8 +60,38 @@ const HackerNewsAlgoliaKit = new KitBuilder({
 			...story,
 		});
 	},
+	async search(
+		inputs: InputValues & HackerNewAlgoliaSearchParameters
+	): Promise<OutputValues & { hits: PostItem[]; }> {
+		const {query, tags, numericFilters, page} = inputs;
+		const url = new URL("https://hn.algolia.com/api/v1/search");
+		url.searchParams.set("query", query);
+		if (tags) {
+			url.searchParams.set("tags", tags.join(","));
+		}
+		if (numericFilters) {
+			url.searchParams.set(
+				"numericFilters",
+				numericFilters
+					.map((filter) => {
+						return `${filter.field}${filter.operator}${filter.value}`;
+					})
+					.join(",")
+			);
+		}
+		if (page) {
+			url.searchParams.set("page", page.toString());
+		}
+		const response = await fetch(url.toString());
+		const {hits} = (await response.json()) as unknown as {
+			hits: PostItem[];
+		};
+		return Promise.resolve({
+			algoliaUrl: url.toString(),
+			hits,
+		});
+	},
 });
 
 export type HackerNewsAlgoliaKit = InstanceType<typeof HackerNewsAlgoliaKit>;
-export { HackerNewsAlgoliaKit };
 export default HackerNewsAlgoliaKit;
