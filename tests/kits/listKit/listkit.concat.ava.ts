fcd4599058c08f40b55702c0ed4213616c34c61e
Joseph Mearman <joseph@mearman.co.uk>
2023-11-24 08:26:34 +0000

    Add tests for listkit.concat functionality
    - Added tests to verify the functionality of listkit.concat in combining arrays and strings.
    - The first test combines 3 arrays and checks if the resulting array is correct.
    - The second test combines an array and a string, ensuring that the resulting array is as expected.

--- tests/kits/listKit/listkit.concat.ava.ts
+++ tests/kits/listKit/listkit.concat.ava.ts
@@ -36,12 +36,83 @@ test("listkit.concat", async (t) => {
 		b: ["John"],
 	});
 
-	// this should work for strings because it's just a sequence of characters
-	const result2 = await board.runOnce({
-		a: "hello",
+	t.deepEqual(result["list"], ["hello", "John"]);
+});
+
+test("listkit.concat can combine 3 arrays", async (t) => {
+	const board = new Board({
+		title: "Exadev",
+		description: "Exadev Breadboard Kit",
+		version: "0.0.1",
+	});
+	const listKit = board.addKit(ListKit);
+
+	const input = board.input({
+		$id: "input",
+		schema: {
+			type: "object",
+			properties: {
+				text: {
+					type: "string",
+					title: "Text",
+					description: "concat",
+				},
+			},
+		},
+	});
+
+	const concat = listKit.concat();
+
+	input.wire("->a", concat);
+	input.wire("->b", concat);
+	input.wire("->c", concat);
+
+	const output = board.output();
+	concat.wire("list->", output);
+
+	const result = await board.runOnce({
+		a: ["hello"],
+		b: ["John"],
+		c: ["Doe"],
+	});
+
+	t.deepEqual(result["list"], ["hello", "John", "Doe"]);
+})
+
+test("listkit.concat can combine an array and a string", async (t) => {
+	const board = new Board({
+		title: "Exadev",
+		description: "Exadev Breadboard Kit",
+		version: "0.0.1",
+	});
+	const listKit = board.addKit(ListKit);
+
+	const input = board.input({
+		$id: "input",
+		schema: {
+			type: "object",
+			properties: {
+				text: {
+					type: "string",
+					title: "Text",
+					description: "concat",
+				},
+			},
+		},
+	});
+
+	const concat = listKit.concat();
+
+	input.wire("->a", concat);
+	input.wire("->b", concat);
+
+	const output = board.output();
+	concat.wire("list->", output);
+
+	const result = await board.runOnce({
+		a: ["hello"],
 		b: "John",
 	});
 
 	t.deepEqual(result["list"], ["hello", "John"]);
-	t.is(result2["list"], "helloJohn");
-});
+})
